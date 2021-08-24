da52241c00367819d6626446aae49d11334a8ca8
dieg4231 <dieg4231@gmail.com>
2021-08-23 23:08:01 -0500

    EKF introduction

--- util/sample_test.m
+++ util/sample_test.m
@@ -1,10 +1,16 @@
 close all
 clear 
 
-o_1 = [ 2; 2; 0 ];
-o = [ 5; 5; .7 ];
-
+start = [ 2; 2; 0 ];
+endd = [ 5; 5; .7 ];
 x = [ 3; 3 ; 0];
+
+
+alpha= [ 0.02
+          0.0001
+          0.001
+          0.00001 ];
+
 
 n = 500;
 puntos = zeros(2,n); 
@@ -12,15 +18,20 @@ puntos = zeros(2,n);
 mu=0;
 sigma=2;
 
+
+
+
 for i=1:n
-  [xk, u ] = sampleOdometry(o_1,o,x);
+  [xk, u ] = sampleOdometry(start,endd,x,alpha);
   puntos(1,i) =  xk(1); %normrnd(mu,sigma);
   puntos(2,i) =  xk(2); %normrnd(mu,sigma-1.5);
 end
-
+r = 0.3;
 
 plot( puntos(1,:) , puntos(2,:),'.');
 
-xlim([0 10])
-ylim([0 10])
-pbaspect([1 1 1])
+hold on
+plot(x(1), x(2), 'ro')
+plot([x(1), x(1) + r*cos(x(3))], [x(2), x(2) + r*sin(x(3))], 'r-' )
+
+
